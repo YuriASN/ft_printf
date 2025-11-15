e33a8ffebd8bd98d6dec431fddbc522b1ca94374
Yuri ASN <yuriasn@hotmail.com>
2025-11-15 16:20:49 +0000

    Changed to work with libft

--- ft_printf_bonus.h
+++ ft_printf_bonus.h
@@ -2,6 +2,7 @@
 # define FT_PRINTF_BONUS_H
 # include <stdarg.h>
 # include <unistd.h>
+# include "libft/libft.h"
 
 int	ft_printf(const char *str, ...);
 int	f_putnbr(int n);
@@ -9,8 +10,6 @@ int	base_conversor(ssize_t n, char *str, ssize_t base);
 int	f_putnbr_u(unsigned int n, char *str, ssize_t base);
 int	f_putnbr_ul(unsigned long n);
 int	f_putchar(char c);
-int	f_strlen(char *str);
-int	f_putstr(char *str);
-int	print_arg(va_list *l, char *c, int *i)
+int	print_arg(va_list *l, char *c, int *i);
 
 #endif
