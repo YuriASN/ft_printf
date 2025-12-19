d28f3fd0cc8b70bbe0c709e6c6bf0a785b0ec3f5
Yuri ASN <yuriasn@hotmail.com>
2025-12-19 16:11:39 +0000

    Fixed unisgned long conversions when using pointer address flag

--- printf_utils.c
+++ printf_utils.c
@@ -18,6 +18,24 @@ static int	base_conversor(ssize_t n, char *str, ssize_t base)
 	return (c);
 }
 
+/* Convert the unsigned long (n) to the base (str) acoording to size of base (base) */
+static int	ul_base_conversor(unsigned long n, char *str, unsigned long base)
+{
+	int	c;
+	int	check;
+
+	c = 0;
+	if (n >= base)
+		c = base_conversor(n / base, str, base);
+	if (c == -1)
+		return (-1);
+	check = write(1, &str[n % base], 1);
+	if (check == -1)
+		return (-1);
+	c += check;
+	return (c);
+}
+
 int	f_putnbr(int n)
 {
 	int			count;
@@ -54,15 +72,13 @@ int	f_putnbr_ul(unsigned long n)
 {
 	int			count;
 	int			check;
-	long long	n2;
 
 	if (n == 0)
 		return (write(1, "(nil)", 5));
 	count = write(1, "0x", 2);
 	if (count == -1)
 		return (-1);
-	n2 = n;
-	check = base_conversor(n2, "0123456789abcdef", 16);
+	check = ul_base_conversor(n, "0123456789abcdef", 16);
 	if (check == -1)
 		return (-1);
 	count += check;
